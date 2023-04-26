6b31d172a737844d31e9132ab27822fe0dd054f3
Anastasiia_Kurenyova <tristana360@gmail.com>
2023-04-26 18:44:37 +0300

    Homework 09 - Size task

---
+++ src/main/java/org/example/lesson09/Main.java
@@ -0,0 +1,10 @@
+package org.example.lesson09;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        for(Size size: Size.values()){
+            System.out.println(size.toString());
+        }
+    }
+}
