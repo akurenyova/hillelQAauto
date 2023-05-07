cca9f731ea5f85af18055ec123df13be83909bb6
Anastasiia_Kurenyova <tristana360@gmail.com>
2023-05-07 14:46:19 +0300

    Homework 05 - Task 01, class User and count amount of purchases

---
+++ src/main/java/ua/ithillel/course02/lesson05/Main.java
@@ -0,0 +1,37 @@
+package ua.ithillel.course02.lesson05;
+
+public class Main {
+    public static void main(String[] args) {
+        User firstUser = new User();
+        User secondUser = new User("secondUser");
+        User thirdUser = new User("thirdUser", "qwertyui");
+
+        System.out.println("\n*** Lesson 5 *** Task 1 ***\n");
+
+        System.out.println("* Purchases of " + firstUser.userName);
+        firstUser.makePurchase(10);
+        firstUser.makePurchase(20);
+        firstUser.makePurchase(30);
+
+        System.out.println("\n* Purchases of " + secondUser.userName);
+        secondUser.makePurchase(5.5);
+        secondUser.makePurchase(15.5);
+        secondUser.makePurchase(25.5);
+
+        System.out.println("\n* Purchases of " + thirdUser.userName);
+        thirdUser.makePurchase(100.25);
+        thirdUser.makePurchase(200.25);
+        thirdUser.makePurchase(300.25);
+
+        System.out.println();
+
+        getUserAmountSpentMoney(firstUser);
+        getUserAmountSpentMoney(secondUser);
+        getUserAmountSpentMoney(thirdUser);
+    }
+
+    public static void getUserAmountSpentMoney(User user) {
+        System.out.println(user.userName + " spent ["
+                + user.amountSpentMoney + "] amount of money");
+    }
+}
---
+++ src/main/java/ua/ithillel/course02/lesson05/User.java
@@ -0,0 +1,89 @@
+package ua.ithillel.course02.lesson05;
+
+public class User {
+
+    private int age;
+    private String password;
+    public String userName;
+    public String email;
+    public boolean isActive;
+    public double amountSpentMoney;
+
+    public User() {
+        this.userName = "anonymous";
+    }
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        setPassword(password);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if ((age > 0) && (age < 120)) {
+            this.age = age;
+        } else {
+            System.out.println("Wrong age");
+        }
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (password.length() >= 8) {
+            this.password = password;
+        } else {
+            System.out.println("Password should be at least 8 symbols");
+        }
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public double getAmountSpentMoney() {
+        return amountSpentMoney;
+    }
+
+    public void setAmountSpentMoney(double amountSpentMoney) {
+        this.amountSpentMoney = amountSpentMoney;
+    }
+
+    public void makePurchase(double purchasePrice) {
+        System.out.println("Purchase price " + purchasePrice);
+        amountSpentMoney += purchasePrice;
+    }
+
+    public void printTotalAmountOfSpentMoney() {
+        System.out.println("Amount of spent money " + amountSpentMoney);
+    }
+}
